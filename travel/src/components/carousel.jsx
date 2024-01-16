d4c8c493536bf124124b6402730dbeeb16d9f8ad
DrewsClues <at869@nau.edu>
2024-01-16 11:58:42 +0900

    read modals added

--- travel/src/components/carousel.jsx
+++ travel/src/components/carousel.jsx
@@ -1,111 +1,130 @@
 import { useState } from 'react';
+import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "../css-sheets/trips-carousel.css";
-import { Form, FormControl, Button, Container, DropdownButton, Dropdown, Card, Carousel } from 'react-bootstrap'
+import { HiOutlineBookOpen } from "react-icons/hi";
+import { PiGlobeHemisphereWestDuotone } from "react-icons/pi";
+import { Modal , Card, Carousel } from 'react-bootstrap'
+
+import Read_Nagoya from './read-modals/Read_Nagoya';
+import Read_Matsumoto from './read-modals/Read-Matsumoto';
+import Read_Kamikochi from './read-modals/Read-Kamikochi';
 
 
 function ControlledCarousel() {
 
+    
   return (
-    <Carousel className='trip-carousel'>
-                <Carousel.Item interval={5000}>
-                    <img
-                    className = "travel-image"  src="Matsumoto.jpg"
-                    />
-                    <Carousel.Caption>
-                    <h3 className='home_caption'>Matsumoto, Japan</h3>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item interval={5000}>
-                    <img
-                    className = "travel-image"  src="Nagoya.jpg"
-                    />
+        <div className='carousel-div'> 
 
-                    <Carousel.Caption>
-                    <h3 className='home_caption'>Nagoya, Japan</h3>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item interval={5000}>
-                    <div>
-                    <img className = "travel-image" src="Kamikochi.jpg"></img> 
-                    </div>
-                    <Carousel.Caption>
-                    <h3 className='home_caption'>Kamikochi, Japan</h3>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item interval={5000}>
-                    <div>
-                    <img className = "travel-image" src="Fukuoka.jpg"></img> 
-                    </div>
-                    <Carousel.Caption>
-                    <h3 className='home_caption'>Fukuoka, Japan</h3>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item interval={5000}>
-                    <div>
-                    <img className = "travel-image" src="Seoul.jpg"></img> 
-                    </div>
-                    <Carousel.Caption>
-                    <h3 className='home_caption'>Seoul, South Korea</h3>
-                    </Carousel.Caption>
-                </Carousel.Item>
+            <Carousel fade className='trip-carousel'>
+                        <Carousel.Item interval={5000}>
+                            <img
+                            className = "travel-image"  src="Matsumoto.jpg"
+                            />
+                            <Carousel.Caption>
 
-                <Carousel.Item interval={5000}>
-                    <div>
-                    <img className = "travel-image" src="Jiufen.jpg"></img> 
-                    </div>
-                    <Carousel.Caption>
-                    <h3 className='home_caption'>Jiufen, Taiwan</h3>
-                    </Carousel.Caption>
-                </Carousel.Item>
+                            <Read_Matsumoto/>
 
-                <Carousel.Item interval={5000}>
-                    <div>
-                    <img className = "travel-image" src="Gwangju.jpg"></img> 
-                    </div>
-                    <Carousel.Caption>
-                    <h3 className='home_caption'>Gwangju, South Korea</h3>
-                    </Carousel.Caption>
-                </Carousel.Item>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item interval={5000}>
+                            <img
+                            className = "travel-image"  src="Nagoya.jpg"
+                            />
 
-                <Carousel.Item interval={5000}>
-                    <div>
-                    <img className = "travel-image" src="Bisbee.jpg"></img> 
-                    </div>
-                    <Carousel.Caption>
-                    <h3 className='home_caption'>Bisbee, Arizona</h3>
-                    </Carousel.Caption>
-                </Carousel.Item>
+                            <Carousel.Caption>
 
-                <Carousel.Item interval={5000}>
-                    <div>
-                    <img className = "travel-image" src="Osaka.jpg"></img> 
-                    </div>
-                    <Carousel.Caption>
-                    <h3 className='home_caption'>Osaka, Japan</h3>
-                    </Carousel.Caption>
-                </Carousel.Item>
+                            <Read_Nagoya/>
 
-                <Carousel.Item interval={5000}>
-                    <div>
-                    <img className = "travel-image" src="White-Sands.jpg"></img> 
-                    </div>
-                    <Carousel.Caption>
-                    <h3 className='home_caption'>White Sands, New Mexico</h3>
-                    </Carousel.Caption>
-                </Carousel.Item>
+                            </Carousel.Caption>
+                        </Carousel.Item>
 
-                <Carousel.Item interval={5000}>
-                    <div>
-                    <img className = "travel-image" src="Taipei.jpg"></img> 
-                    </div>
-                    <Carousel.Caption>
-                    <h3 className='home_caption'>Taipei, Taiwan</h3>
-                    </Carousel.Caption>
-                </Carousel.Item>
+                        <Carousel.Item interval={5000}>
+                            <div>
+                            <img className = "travel-image" src="Kamikochi.jpg"></img> 
+                            </div>
+                            <Carousel.Caption>
+
+                            <Read_Kamikochi/>
+
+                            </Carousel.Caption>
+                        </Carousel.Item>
+
+                        <Carousel.Item interval={5000}>
+                            <div>
+                            <img className = "travel-image" src="Fukuoka.jpg"></img> 
+                            </div>
+                            <Carousel.Caption>
+                            <h3 className='home_caption'>Fukuoka, Japan</h3>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item interval={5000}>
+                            <div>
+                            <img className = "travel-image" src="Seoul.jpg"></img> 
+                            </div>
+                            <Carousel.Caption>
+                            <h3 className='home_caption'>Seoul, South Korea</h3>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+
+                        <Carousel.Item interval={5000}>
+                            <div>
+                            <img className = "travel-image" src="Jiufen.jpg"></img> 
+                            </div>
+                            <Carousel.Caption>
+                            <h3 className='home_caption'>Jiufen, Taiwan</h3>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+
+                        <Carousel.Item interval={5000}>
+                            <div>
+                            <img className = "travel-image" src="Gwangju.jpg"></img> 
+                            </div>
+                            <Carousel.Caption>
+                            <h3 className='home_caption'>Gwangju, South Korea</h3>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+
+                        <Carousel.Item interval={5000}>
+                            <div>
+                            <img className = "travel-image" src="Bisbee.jpg"></img> 
+                            </div>
+                            <Carousel.Caption>
+                            <h3 className='home_caption'>Bisbee, Arizona</h3>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+
+                        <Carousel.Item interval={5000}>
+                            <div>
+                            <img className = "travel-image" src="Osaka.jpg"></img> 
+                            </div>
+                            <Carousel.Caption>
+                            <h3 className='home_caption'>Osaka, Japan</h3>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+
+                        <Carousel.Item interval={5000}>
+                            <div>
+                            <img className = "travel-image" src="White-Sands.jpg"></img> 
+                            </div>
+                            <Carousel.Caption>
+                            <h3 className='home_caption'>White Sands, New Mexico</h3>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+
+                        <Carousel.Item interval={5000}>
+                            <div>
+                            <img className = "travel-image" src="Taipei.jpg"></img> 
+                            </div>
+                            <Carousel.Caption>
+                            <h3 className='home_caption'>Taipei, Taiwan</h3>
+                            </Carousel.Caption>
+                        </Carousel.Item>
 
 
-            </Carousel>
+                    </Carousel>
+                </div>
 
   );
 }
